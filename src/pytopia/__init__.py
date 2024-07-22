70e9bd5afb5f551c0793b394a68e7c1ffbacc922
Blake Larson <wokeblokemail@gmail.com>
2024-07-22 15:05:56 -0400

    Initial commit!

---
+++ src/pytopia/__init__.py
@@ -0,0 +1,13 @@
+"""
+Pytopia Package
+
+<Description of this package>
+
+Submodules:
+- 
+"""
+
+# Expose submodules
+
+# Define what gets imported with `from pytopia import *`
+__all__ = []
---
+++ tests/quick_test.py
@@ -0,0 +1,11 @@
+import pytest
+
+# Test function for an operation
+@pytest.mark.quicktest
+def test_operation():
+    pass  # TODO: Implement operation test
+
+# Test function for a component
+@pytest.mark.quicktest
+def test_component():
+    pass  # TODO: Implement component test
